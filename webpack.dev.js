ecfa074e2e1bcf36ee1cb2dcfda8e13ccc26be79
Sandeep Singh <sandeepsingh2194@gmail.com>
2020-04-23 03:14:47 +0530

    fixed them all

--- webpack.dev.js
+++ webpack.dev.js
@@ -7,16 +7,19 @@ module.exports = {
     entry: './src/client/index.js',
     mode: 'development',
     devtool: 'source-map',
+    output: {
+        libraryTarget: 'var',
+        library: 'Client'
+    },
     module: {
-        rules: [
-            {
+        rules: [{
                 test: '/\.js$/',
                 exclude: /node_modules/,
                 loader: "babel-loader"
             },
             {
                 test: /\.scss$/,
-                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+                use: ['style-loader', 'css-loader', 'sass-loader']
             }
         ]
     },
